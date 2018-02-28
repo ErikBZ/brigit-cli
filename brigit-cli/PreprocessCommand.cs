adf20c95432f2c85602eb7909a75d570db584839
Erik Zapata <zapatabrandon@gmail.com>
2018-02-27 22:59:45 -0500

    Initializes project and adds some code to play around with ManyConsole

---
+++ brigit-cli/PreprocessCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ManyConsole;
+
+namespace brigit_cli
+{
+	class PreprocessCommand : ConsoleCommand
+	{
+		public string FileName { get; set; }
+
+		public PreprocessCommand()
+		{
+			// "this" is not necessary and according to MSDN best practices
+			// should be avoided
+			IsCommand("Preprocess", "Command makes sure that the following text file compiles");
+			HasRequiredOption("f|file=", "The tome file to compile", f => FileName = f);
+			// if i need any other options i'll add them here
+		}
+
+		public override int Run(string[] remainingArguments)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ brigit-cli/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ManyConsole;
+
+namespace brigit_cli
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
